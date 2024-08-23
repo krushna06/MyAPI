1efd23a24f6021f71cc38d1ad237a5194f557b5f
n0step_ <krushnavyas06@gmail.com>
2024-08-23 09:16:05 +0000

    fix: added lanyard for spotify status

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -2,8 +2,24 @@ const axios = require('axios');
 
 exports.getProfile = async (req, res) => {
   try {
-    const response = await axios.get('http://dcdn.n0step.xyz:8787/profile/853620650592567304');
-    res.json(response.data);
+    const profileResponse = await axios.get('http://dcdn.n0step.xyz:8787/profile/853620650592567304');
+    const profileData = profileResponse.data;
+
+    const lanyardResponse = await axios.get('https://api.lanyard.rest/v1/users/853620650592567304');
+    const lanyardData = lanyardResponse.data;
+
+    const activities = lanyardData.data.activities || [];
+    const spotify = lanyardData.data.spotify || null;
+
+    const combinedData = {
+      profile: profileData,
+      lanyard: {
+        activities,
+        spotify
+      }
+    };
+
+    res.json(combinedData);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch profile data' });
   }
