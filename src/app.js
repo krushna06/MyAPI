0322d97f2f7c8062855a37caa48dc282a6a9ce3a
n0step_ <krushnavyas06@gmail.com>
2024-08-23 08:46:48 +0000

    fix: change default routes

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const spotifyRoutes = require('./routes/spotifyRoutes');
 
 const app = express();
 
-app.use('/weather', weatherRoutes);
-app.use('/dprofile', profileRoutes);
-app.use('/spotify', spotifyRoutes);
+app.use('/api/v1/weather', weatherRoutes);
+app.use('/api/v1/dprofile', profileRoutes);
+app.use('/api/v1/spotify', spotifyRoutes);
 
 module.exports = app;
