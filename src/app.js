3215a3b677dbd76020f8f7f9d7106f830f442713
n0step_ <krushnavyas06@gmail.com>
2024-08-23 11:53:00 +0000

    fix: cors for portfolio

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const weatherRoutes = require('./routes/weatherRoutes');
 const profileRoutes = require('./routes/profileRoutes');
 const spotifyRoutes = require('./routes/spotifyRoutes');
 const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/api/v1/weather', weatherRoutes);
 app.use('/api/v1/dprofile', profileRoutes);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 const app = require('./app');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3006;
+const cors = require('cors');
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
